664314a266aa2384518395932c9f4ded65e68269
Arron Alsop <alsop.arron@gmail.com>
2023-12-19 12:23:26 -0800

    'wip'

--- api/server.js
+++ api/server.js
@@ -1,27 +1,27 @@
 // build your server here and require it from index.js
 const express = require('express')
-
-// const projectRouter = require('./project/router')
-// const resourceRouter = require('./resource/router')
+const server = express()
+const projectRouter = require('./project/router')
+const resourceRouter = require('./resource/router')
 // const taskRouter = require('./task/router')
 
-const server = express()
+
 
 server.use(express.json())
 
-// server.use('/api/projects', projectRouter)
-// server.use('/api/resources', resourceRouter)
+server.use('/api/projects', projectRouter)
+server.use('/api/resources', resourceRouter)
 // server.use('/api/tasks', taskRouter)
 
 server.use('*', (req, res, next) => {
     next({ status: 400, message: `me no workie` })
 })
 
-// server.use((err, req, res, next) => {
-//     res.status(err.status || 500).json({
-//         message: `i'm having troubles with the recipes router`,
-//         stack: err.stack
-//     })
-// })
+server.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: `Error: ${err.message}`,
+        stack: err.stack
+    })
+})
 
 module.exports = server
